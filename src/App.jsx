f4362d5fefd23b8328cbb4f8879dcf53195ee7f9
FinalRaye <115549558+FinalRaye@users.noreply.github.com>
2025-07-17 04:44:00 +0800

    Implement routing in App.jsx and main.jsx Add Medications and CareRecipient components with styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
+import { Routes, Route } from 'react-router-dom';
 import Header from './Header/Header';
-import ForumBody from './Forum/ForumBody/ForumBody'; 
+import ForumBody from './Forum/ForumBody/ForumBody';
+import MedicationsPage from './Medications/MedicationsPage/MedicationsPage';
+
 function App() {
   return (
-    <>
-      <div
-        style={{
-          backgroundImage: "linear-gradient(to bottom, #FFFDF9 0%, #F1C5C0 50%)",
-        }}
-      >
-        <Header />
-        <ForumBody/>
-      </div>
-    </>
+    <div
+      style={{
+        backgroundImage: "linear-gradient(to bottom, #FFFDF9 75%, #F1C5C0 100%)",
+      }}
+    >
+      <Header />
+
+      <Routes>
+
+        <Route path="/" element={<ForumBody />} />
+
+
+        <Route path="/forum" element={<ForumBody />} />
+
+
+        <Route path="/medications" element={<MedicationsPage />} />
+
+
+      </Routes>
+    </div>
   );
 }
 
