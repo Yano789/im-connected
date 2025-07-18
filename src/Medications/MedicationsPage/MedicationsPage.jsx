9519893a159ffb070444f2f952cdf95143d3c09d
Rayen <seah.mj1@gmail.com>
2025-07-19 05:53:31 +0800

    Add MedicationLogging MedicationDetails components with styles
    Refines the design CSS of words and lines

--- src/Medications/MedicationsPage/MedicationsPage.jsx
+++ src/Medications/MedicationsPage/MedicationsPage.jsx
@@ -1,18 +1,53 @@
 import React, { useState } from 'react';
 import './MedicationsPage.css';
 import CareRecipientList from '../CareRecipientList/CareRecipientList'; 
-// import MedicationLogging from '../MedicationLogging/MedicationLogging';
-// import MedicationDetails from '../MedicationDetails/MedicationDetails';
+import MedicationLogging from '../MedicationLogging/MedicationLogging';
+import { set } from 'mongoose';
+import MedicationDetails from '../MedicationDetails/MedicationDetails';
 
 // --- DUMMY DATA (stays the same) ---
 const careRecipientsData = [
-    { id: 1, name: 'James Tan', medications: [
-        { id: 'med1', name: 'Metformin XR 500mg', dosage: '1st pill @ 10am', taken: true, schedule: ['Morning'], usedTo: 'Manages Type 2 Diabetes Mellitus.', sideEffects: 'May cause Drowsiness', image: 'path/to/metformin_image.jpg' },
-        { id: 'med2', name: 'Glucosamine Sulfate 500 mg', dosage: '1st pill @ 10am', taken: true, schedule: ['Morning'], usedTo: 'Treats osteoarthritis.', sideEffects: 'Nausea, heartburn.', image: 'path/to/glucosamine_image.jpg' },
-        { id: 'med3', name: 'Gliclazide (Diamicron) 30 mg', dosage: '1st pill @ 10am', taken: false, schedule: ['Morning'], usedTo: 'Controls blood sugar.', sideEffects: 'Hypoglycemia.', image: 'path/to/gliclazide_image.jpg' },
-    ]},
+    {
+        id: 1, name: 'James Tan', medications: [
+            { 
+                id: 'med1', 
+                name: 'Metformin XR 500mg', 
+                dosages: [
+                    { time: '1st pill @ 10am', taken: true },
+                    { time: '2nd pill @ 12pm', taken: false },
+                    { time: '3rd pill @ 6pm', taken: true }
+                ],
+                usedTo: 'Manages Type 2 Diabetes Mellitus.', 
+                sideEffects: 'May cause Drowsiness', 
+                image: 'https://i.imgur.com/8m2bAOr.jpeg' 
+            },
+            { 
+                id: 'med2', 
+                name: 'Glucosamine Sulfate 500 mg', 
+                dosages: [
+                    { time: '1st pill @ 10am', taken: true }
+                ],
+                usedTo: 'Treats osteoarthritis.', 
+                sideEffects: 'Nausea, heartburn.', 
+                image: 'https://i.imgur.com/8m2bAOr.jpeg' 
+            },
+            { 
+                id: 'med3', 
+                name: 'Gliclazide (Diamicron) 30 mg', 
+                dosages: [
+                    { time: '1st pill @ 10am', taken: true },
+                    { time: '2nd pill @ 12pm', taken: false },
+                    { time: '3rd pill @ 6pm', taken: false },
+                    { time: '4th pill @ 10pm', taken: false }
+                ],
+                usedTo: 'Controls blood sugar.', 
+                sideEffects: 'Hypoglycemia.', 
+                image: 'https://i.imgur.com/8m2bAOr.jpeg' 
+            },
+        ]
+    },
     { id: 2, name: 'Amelia Tan', medications: [
-        { id: 'med4', name: 'Aspirin 100mg', dosage: '1st pill @ 8am', taken: false, schedule: ['Morning'], usedTo: 'Pain relief, blood thinner.', sideEffects: 'Upset stomach.', image: 'path/to/aspirin_image.jpg' }
+        { id: 'med4', name: 'Aspirin 100mg', dosages: [{ time: '1st pill @ 8am', taken: false }], usedTo: 'Pain relief, blood thinner.', sideEffects: 'Upset stomach.', image: 'https://i.imgur.com/8m2bAOr.jpeg' }
     ]}
 ];
 // ------------------------------------
@@ -44,18 +79,14 @@ function MedicationsPage() {
 
             {/* -- Row 2 -- */}
             <div className="grid-item-logging">
-                {/* Placeholder for MedicationLogging */}
-                <div style={{ border: '2px dashed #ccc', padding: '1rem', textAlign: 'center' }}>
-                    <h3>Medication Logging</h3>
-                    <p>(This is where the medications will be listed)</p>
-                </div>
+                <MedicationLogging
+                    medications={selectedRecipient.medications}
+                    onSelect={setSelectedMedication}
+                    selectedMedicationId={selectedMedication ? selectedMedication.id : null}
+                />
             </div>
             <div className="grid-item-details">
-                {/* Placeholder for MedicationDetails */}
-                <div style={{ border: '2px dashed #ccc', padding: '1rem', textAlign: 'center', height: '400px' }}>
-                    <h3>Medication Details</h3>
-                    <p>(Details will appear here)</p>
-                </div>
+                <MedicationDetails medication={selectedMedication} />
             </div>
         </div>
     );
