cea70fc079126b34e81e582f11afb6e4b5bce938
etherealseven <hoonkiahyen1606@gmail.com>
2025-08-07 14:37:24 +0800

    change to pointer on hover

--- src/Splashscreen/Splashscreen.jsx
+++ src/Splashscreen/Splashscreen.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import HeartLogo from "../assets/HeartLogo.png";
 import SplashChars from "../assets/SplashscreenCharacters.png";
